8b214ca4fc11a2d8ae83cb0cec67e07f40f2c0e8
hachim112 <hachim.fernane.univ@gmail.com>
2026-06-13 22:06:26 +0100

    STATIC

--- clinique_elita/wsgi.py
+++ clinique_elita/wsgi.py
@@ -8,10 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinique_elita.settings')
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
 from vercel_setup import run_vercel_setup
 
